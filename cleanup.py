cac5b348a94f0c674378e4580c2b9ee6606a0787
diop <fodesdiop@gmail.com>
2019-02-19 13:46:32 -0800

    implement cleanup file

--- cleanup.py
+++ cleanup.py
@@ -1,9 +1,25 @@
+def cleanup(source_text='fishes.txt'):
+    '''
+    Cleanes the source text document.
+    '''
+    with open(source_text, 'r') as f:
+        text_body = f.read()
 
+    removed_new_lines = text_body.replace('\n', '')
+    removed_commas = removed_new_lines.replace(', ', ' ')
+    removed_periods = removed_commas.replace('.', ' ')
+    pruned_source_text = removed_periods.split(' ')
 
-#%% 
-x = [3,4,5]
-print(x)
-import matplotlib.pyplot as plt 
-y = [5,6,7]
-plt.plot(x, y)
+    for index in range(len(pruned_source_text)):
+        word = pruned_source_text[index].lower()
+        pruned_source_text[index] = word
 
+    for word in pruned_source_text:
+        if word == '':
+            pruned_source_text.remove(word)
+
+    return pruned_source_text
+
+if __name__ == '__main__':
+    text = cleanup()
+    print(text)
