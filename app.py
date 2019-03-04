381c7fac69096e01668e90f0df39f1c56e73e4fe
diop <fodesdiop@gmail.com>
2019-03-04 14:44:55 -0800

    update app.py for heroku

--- app.py
+++ app.py
@@ -8,5 +8,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def main():
     return 'Only a fool tests the depth of a river with both feet. - African Proverb'
+
+if __name__ == '__main__':
+    app.run()
