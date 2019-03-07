2b9e44c1b3841d1a19fd7d793e26a8d98253c54b
diop <fodesdiop@gmail.com>
2019-03-07 01:26:10 -0800

    implement basic markov chain

--- markov.py
+++ markov.py
@@ -1,40 +1,39 @@
+
+# Code attribution: https://github.com/anselb/Tweet-Generator/blob/master/source/markov.py 
 from dictogram import Dictogram
-import random 
-from collections import deque
-import re
+import sample
+from random import randint
 
-def generate_random_start(model):
-    # To just generate any starting word uncomment line:
-    # return random.choice(model.keys())
+def markov_dictograms(text):
+    arr = text.split()
+    dico = {}
 
-    # To generate a "valid" starting word use:
-    # Valid starting words are words that started a sentence in the corpus.
-    if 'END' in model:
-        seed_word = 'END'
-        while seed_word == 'END':
-            seed_word == model['END'].return_weighted_random_word()
-        return seed_word
-        return random.choice(model.keys())
+    for word_index in range(len(arr) - 1):
+        current_word = arr[word_index]
+        next_word = arr[word_index + 1]
 
-def generate_random_sentence(length, markov_model):
-    current_word = generate_random_start(markov_model)
-    sentence = [current_word]
-    for i in range(o, length):
-        current_dictogram = markov_model[current_word]
-        random_weighted_word = current_dictogram.return_weighted_random_word()
-        current_word = random_weithted_word
-        sentecen.append(current_word)
-    sentence[0] = sentence[0].capitalize()
-    return ' '.jion(sentence) + '.'
-    return sentence
+        if current_word in dico:
+            dico[current_word].add_count(next_word)
+        else:
+            dico[current_word] = Dictogram([next_word])
+    return(dico)
 
-# def make_markov_model(data):
-#     markov_model = dict()
+def markov_sample(dictogram):
+    return sample.weighted_random_selection(dictogram)
 
-#     for i in range(0, len(data)-1):
-#         if data[i] in markov_model:
-#             # We just have to append to the existing histogram.
-#             markov_model[data[i]].update([data[i+1]])
-#         else:
-#             markov_model[data[i]] = Dictogram([data[i+1]])
-#     return markov_model
+def markov_chain(dictogram_dictionary):
+    dictionary_keys = [key for key, value in dictogram_dictionary.items()]
+    sentence_array = [dictionary_keys[randint(0, len(dictionary_keys)- 1)]]
+
+    for word_index in range(10):
+        word_dictogram = dictogram_dictionary[sentence_array[word_index]]
+        next_word = markov_sample(word_dictogram)
+        sentence_array.append(next_word)
+
+    return ' '.join(sentence_array)
+
+if __name__ == '__main__':
+    fish_text = 'one fish two fish red fish blue fish'
+
+    fish_markov_dictionary = markov_dictograms(fish_text)
+    print(markov_chain(fish_markov_dictionary))
